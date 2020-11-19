5a66000356340c21887dcb693a48961bfb0f7bfd
Wayne Cho <waynecho2004@gmail.com>
2020-11-19 16:27:23 -0500

    add search to app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import Search from './positions/components/Search'
+
 class App extends  Component{
   render(){
 
     return(
       <>
-      <h1>Job Search</h1>
-<div>
-
-<button>Search</button>
-</div>
+        <Search 
+          onSubmit = {this.handleSubmit}
+        />
+        <h3>- List of Positions</h3>
       </>
     )
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('description: ' + e.target.description.value)
+    console.log('locatiion: ' + e.target.location.value)
+
+    // STEP1: axio search function
+    // STEP2: setState for positions
   }
 }
 
