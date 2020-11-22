b0fdade19220775a78abfd830fb88c9905847676
Vaishali Dighe-Phanse <vaishphanse@gmail.com>
2020-11-22 16:15:12 -0500

    AboutUs Page

--- src/pages/components/About.js
+++ src/pages/components/About.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+//https://materializecss.com/images/sample-1.jpg
+
+import AboutDB from '../../AboutDB';
+//width: 18rem;
 const About = () => {
+    const trail = AboutDB.us.map(t => {
+        return (
+
+            <div className="card" style={{ width: 15 + 'rem' }}>
+                <img className="card-img-top" src={t.imgSmall} />
+                <div className="card-body">
+                    <div className="card-title">{t.name}</div>
+                    <div className="card-description">{t.description} </div>
+                </div>
+            </div>
+        )
+    });
     return (
         <>
-        <h3> About US</h3>
-        <ul>
-            <li>We are building the TechGig!!</li>
-        </ul>
+
+            <div className="row">
+                {trail}
+            </div>
+
+
         </>
     )
 }
-export default About;
+export default About
