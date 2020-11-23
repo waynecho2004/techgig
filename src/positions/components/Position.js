7d525e89457efcab7e1b5993941572626e6efe39
“daniel_penumala” <penumala.gmail.com>
2020-11-23 14:59:20 -0500

    updates to position

--- src/positions/components/Position.js
+++ src/positions/components/Position.js
@@ -1,33 +1,28 @@
-import React, { Component } from 'react';
-
-
+import React, { Component } from "react";
 class Position extends Component {
   constructor(props) {
     super(props);
-    this.state = {        
-    }
-}
+  }
   render() {
     // Write Filter method
-    console.log(this.props);
+    const position = this.props.positions.filter((position) => {
+      return position.id === this.props.id;
+    });
+
     return (
       <div className="position">
-        <p> Renders</p>
         {/* Job ID,type,URl,Created_at,Company,Company URL,,location,title,description*/}
-        <h4>{this.props.position.type}</h4>
-        <a href={this.props.url}></a>
-        <h4>{this.props.created_at}</h4>
-        <h4>{this.props.company}</h4>
-        <h4>{this.props.company_url}</h4>
-        <h4>{this.props.location}</h4>
-        <h4>{this.props.title}</h4>
-        <a href={this.props.url}></a>
-        <p>
-        {this.props.description}
-        </p>
+        <h4>Type: {position.title}</h4>
+        <a href={position.url}> GITHUB Posting </a>
+        <h4>Created: {position.created_at}</h4>
+        <h4>Company Name: {position.company}</h4>
+        <a href={position.company_url}>Company URL</a>
+        <h4>Location: {position.location}</h4>
+        <h4>Job Title: {position.title}</h4>
+        <p>Job Description: {position.description}</p>
+        <a href={position.how_to_apply}>How To Apply</a>
       </div>
-    )
+    );
   }
-  
 }
 export default Position;
