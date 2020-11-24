ea08180ac42197d1f8a22c502319acd260a84fb9
Vaishali Dighe-Phanse <vaishphanse@gmail.com>
2020-11-23 23:28:07 -0500

    ReactBootstrap

--- src/pages/components/About.js
+++ src/pages/components/About.js
@@ -1,31 +1,35 @@
 import React from 'react'
 import AboutDB from '../../AboutDB';
-import '../../css/styles.css';
+
+import { Card, Row, Col, Container } from 'react-bootstrap';
 
 const About = () => {
     const us = AboutDB.us.map((t) => {
-        
+
         return (
-            <div key = {t.id} className="card" style={{ width: 15 + 'rem' }}>
-                <img className="card-img-top" src={t.imgSmall} alt="/logo192.png" />
-                <div className="card-body">
-                    <div className="card-title">{t.name}</div>
-                    <div className="card-description">{t.description} </div>
-                </div>
-            </div>
+            <Card key={t.id} style={{ width: 15 + 'rem' }}>
+                <Card.Img variant="top" src={t.imgSmall} alt="/logo192.png" />
+            
+                <Card.Body>
+                    <Card.Title>{t.name}</Card.Title>
+                    <Card.Text>{t.description} </Card.Text>
+                </Card.Body>
+            </Card>
         )
     });
     return (
         <>
-            <div className="row">
-                <div className="col-sm-12">
-                    <h1>Change your world Using TechGig.</h1>
-                    <p>Set your tech career in motion with TechGig</p>
-                </div>
-            </div>
-            <div className="row">
-                {us}
-            </div>
+            <Container>
+                <Row className="col-sm-12">
+                    <Col >
+                        <h1>Change your world Using TechGig.</h1>
+                        <p>Set your tech career in motion with TechGig</p>
+                    </Col>
+                </Row>
+                <Row>
+                    {us}
+                </Row>
+            </Container>
         </>
     )
 }
