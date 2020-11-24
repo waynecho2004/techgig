9653997c66ee4cd57f91ef6d778a6516c817b6c3
Vaishali Dighe-Phanse <vaishphanse@gmail.com>
2020-11-24 10:22:03 -0500

    Render the Postions in tabular format

--- src/positions/components/PositionRow.js
+++ src/positions/components/PositionRow.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
-////import LikeButton from './LikeButton';
-//import HeartButton from './HeartButton';
-//import CheckButton from './CheckButton';
+import { Row, Col, Container, Form, Button } from 'react-bootstrap';
 import LikeButton from './ClickableButton';
 
 const PositionRow = (props) => {
@@ -18,33 +16,28 @@ const PositionRow = (props) => {
         index,
         handleAddFavoriteClick
     } = props;
- // console.log(id);
+    // console.log(id);
     return (
         <>
-            <div className="position-item">
-                <div className="company-logo">
-                    <img src={company_logo} alt={company} width="100" height="100" />
-                </div>
-                <div className="position-info" >
-                    <div className="position-title">
+        <tr key={id}>
+               <td><img src={company_logo} alt={company} width="100" height="100" /></td>
+               <td><div className="position-title">
                         <Link to={'/positions/details/' + id}>{title}</Link>
                     </div>
                     <div className="position-location">
                         {location} | {type}
                     </div>
                     <div className="company-name">{company}</div>
-                </div>
-                <div className="post-info">
-                    <div className="post-time">
+                    </td>
+               <td><div className="post-time">
                         Posted {moment(new Date(created_at)).fromNow()}
-                    </div>
-                </div>
-                <div className="post-info">
-                    <div className="post-like">               
-                        <LikeButton onClick={handleAddFavoriteClick} buttonTitle='Like'/>
-                    </div>
-                </div>
-            </div>
+                    </div></td>
+               <td> <div className="post-like">
+                        <LikeButton onClick={handleAddFavoriteClick} buttonTitle='Like' />
+                    </div></td>
+             </tr>
+       
+            
         </>
     );
 }
